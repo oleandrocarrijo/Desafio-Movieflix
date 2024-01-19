2b568236aff3a8eb28d9dabb94562715f2eb5ad8
Leandro Carrijo <7leandrobatista@gmail.com>
2024-01-19 15:12:48 -0300

    feat: created MovieRepository, Service and Controller

---
+++ src/main/java/com/devsuperior/movieflix/controllers/MovieController.java
@@ -0,0 +1,14 @@
+package com.devsuperior.movieflix.controllers;
+
+import com.devsuperior.movieflix.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieController {
+
+	@Autowired
+	private MovieService movieService;
+}
