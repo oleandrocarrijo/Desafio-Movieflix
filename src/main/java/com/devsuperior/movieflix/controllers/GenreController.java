08f626e08845575e1afa8e74f06c3271a0d45e60
Leandro Carrijo <7leandrobatista@gmail.com>
2024-01-19 15:08:05 -0300

    feat: implemented findAll Genres

--- src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
+++ src/main/java/com/devsuperior/movieflix/controllers/GenreController.java
@@ -1,9 +1,17 @@
 package com.devsuperior.movieflix.controllers;
 
+import com.devsuperior.movieflix.dto.GenreDTO;
 import com.devsuperior.movieflix.services.GenreService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/genres")
@@ -11,4 +19,11 @@ public class GenreController {
 
 	@Autowired
 	private GenreService genreService;
+
+	@PreAuthorize("hasAnyRole('VISITOR', 'MEMBER')")
+	@GetMapping
+	public ResponseEntity<List<GenreDTO>> findAll() {
+		List<GenreDTO> dtos = genreService.findAll();
+		return ResponseEntity.ok().body(dtos);
+	}
 }
