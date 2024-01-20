3ab5b816b74d3605280721ffd67e97dcdd29ed93
Leandro Carrijo <7leandrobatista@gmail.com>
2024-01-20 14:19:21 -0300

    feat: created ReviewRepository, Service and Controller

---
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,11 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    @Autowired
+    private ReviewService reviewService;
+}
